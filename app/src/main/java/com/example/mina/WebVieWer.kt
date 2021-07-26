8e6c67bb672837c3fc189194465b225513e7c5db
AngeGarry <50191576+AngeGarry@users.noreply.github.com>
2021-07-26 16:49:58 -0400

    jr 2

--- app/src/main/java/com/example/mina/WebVieWer.kt
+++ app/src/main/java/com/example/mina/WebVieWer.kt
@@ -1,25 +1,29 @@
 package com.example.mina
 
+import android.app.ProgressDialog
 import android.content.Context
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
 import android.view.WindowManager
-import android.webkit.JavascriptInterface
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
 import android.widget.Toast
 import androidx.webkit.WebViewCompat
 
 class WebVieWer : AppCompatActivity() {
 
     lateinit var myWebView : WebView
+    lateinit var context : Context
+    lateinit var upLoadListener: DownloadListener
+    var readAccess = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         //this is for hiding status bar
-        this.getWindow().setFlags(
+        this.window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN ,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
@@ -27,24 +31,73 @@ class WebVieWer : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_viewer)
 
+        //find layout
         myWebView = findViewById(R.id.webview)
+
+        // Enable responsive layout
+        myWebView.settings.useWideViewPort = true
+
+        // Zoom out if the content width is greater than the width of the viewport
+        myWebView.settings.loadWithOverviewMode = true
 
         //Add JavaScript interface for popups
         //myWebView.addJavascriptInterface(WebAppInterface(this), "Android")
 
-        //Enable JavaScript
-        myWebView.settings.javaScriptEnabled = true
+        /*myWebView.webChromeClient = object : WebChromeClient(){
 
-        //Handle page navigation
-        myWebView.webViewClient = object : WebViewClient(){
-            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                view?.loadUrl(url!!)
-                return true
+        }*/
+
+        //Default Settings
+        myWebView.apply {
+            //Configure related browser settings
+            this.settings.loadsImagesAutomatically = true
+            this.settings.javaScriptEnabled = true
+            this.settings.allowContentAccess = true
+            this.settings.allowFileAccess = true
+            myWebView.scrollBarStyle = View.SCROLLBARS_INSIDE_OVERLAY
+
+            //Handle url navigation page
+            myWebView.webViewClient = object : WebViewClient() {
+
+                val progressDialog = ProgressDialog(context)
+
+                override fun onPageStarted(view: WebView? , url: String? , favicon: Bitmap?) {
+                    super.onPageStarted(view , url , favicon)
+                    progressDialog.setTitle("Loading...")
+                    progressDialog.setMessage("Please Wait!")
+                    progressDialog.setCancelable(false)
+                    progressDialog.show()
+                }
+
+                override fun onPageCommitVisible(view: WebView? , url: String?) {
+                    super.onPageCommitVisible(view , url)
+                    if (progressDialog != null) {
+                        progressDialog.dismiss()
+                    }
+                }
+
+                override fun onReceivedError(
+                    view: WebView? ,
+                    request: WebResourceRequest? ,
+                    error: WebResourceError?
+                ) {
+                    super.onReceivedError(view , request , error)
+                    val path: String = Uri.parse("file:///android_asset/404.html").toString()
+                    myWebView.loadUrl(path)
+
+                }
+
+                override fun shouldOverrideUrlLoading(view: WebView? , url: String?): Boolean {
+                    view?.loadUrl(url!!)
+                    //Toast.makeText(this, url, Toast.LENGTH_SHORT).show()
+                    return true
+                }
             }
-        }
 
-        //Add default URL
-        myWebView.loadUrl("https://quick-computer.herokuapp.com/")
+            //Add default URL
+            myWebView.loadUrl("https://quick-computer.herokuapp.com/")
+
+        }
 
     }
 
